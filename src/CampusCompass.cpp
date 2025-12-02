40010df1c896a1dd34ba7e5cbac6b7025f444c30
Henry Canapp <henrycanapp@ufl.edu>
2025-12-02 17:24:48 -0500

    Completed most helper functions and data reading

--- src/CampusCompass.cpp
+++ src/CampusCompass.cpp
@@ -3,19 +3,197 @@
 #include <string>
 
 
-CampusCompass::CampusCompass() {
-    // initialize your object
+CampusCompass::CampusCompass(const std::string &edges_filepath, const std::string &classes_filepath) {
+    //setting up graph and classes
+    parseCSV(edges_filepath, classes_filepath);
+    //for command parsing
+    commands = {{"insert",0},{"remove",1},{"dropClass",2},{"replaceClass",3},{"removeClass",4},
+        {"toggleEdgesClosure",5},{"checkEdgeStatus",6},{"isConnected",7},{"printShortestEdges",8},
+        {"printStudentZone",9}};
 }
 
-bool CampusCompass::parseCSV(const std::string &edges_filepath, const std::string &classes_filepath) {
-    // return boolean based on whether parsing was successful or not
-    return true;
+std::vector<std::vector<std::string>> CampusCompass::readCSV(const std::string& filename) {
+    //code taken directly from https://medium.com/@ryan_forrester_/reading-csv-files-in-c-how-to-guide-35030eb378ad
+    std::vector<std::vector<std::string>> data;
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return data;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::vector<std::string> row;
+        std::stringstream ss(line);
+        std::string cell;
+
+        while (std::getline(ss, cell, ',')) {
+            if (!cell.empty() && cell.back() == '\r') {
+                cell.pop_back();
+            }
+            row.push_back(cell);
+        }
+
+        data.push_back(row);
+    }
+
+    file.close();
+    return data;
 }
 
-bool CampusCompass::parseCommand(const std::string &command) {
-    // do whatever regex you need to parse validity
-    // hint: return a boolean for validation when testing. For example:
-    bool is_valid = true; // replace with your actual validity checking
+void CampusCompass::parseCSV(const std::string& edges_filepath, const std::string& classes_filepath) {
+    //enters csv data into structures in desired format
 
+    auto edge_data = readCSV(edges_filepath);
+    //data is in the form of (from_id, to_id, from_name, to_name, time)
+    //graph is in the form of <id_from, vector<tuple<id_to, weight/time, closure>>>
+
+    //adding each edge to the corresponding list and setting the closed status to false
+    bool first_line = true;
+    for (auto row : edge_data) {
+        if (first_line) {
+            first_line = false;
+            continue;
+        }
+        int first_id = std::stoi(row[0]);
+        int second_id = std::stoi(row[1]);
+        int weight = std::stoi(row[4]);
+        //adding edge to first id's list
+        if (graph.count(first_id)) {
+            std::vector<std::tuple<int, int, bool>> edges;
+            graph[first_id] = edges;
+        }
+        graph[first_id].emplace_back(second_id, weight, false);
+        //adding edge to second id's list
+        if (graph.count(first_id)) {
+            std::vector<std::tuple<int, int, bool>> edges;
+            graph[first_id] = edges;
+        }
+        graph[first_id].emplace_back(second_id, weight, false);
+    }
+
+    auto classes_data = readCSV(classes_filepath);
+    //data is in the form of (class code, location id, start time, end time)
+    //classes list is in the form of <class code str, id int>
+
+    //adding each edge into the list
+    first_line = true;
+    for (auto row : classes_data) {
+        if (first_line) {
+            first_line = false;
+            continue;
+        }
+        std::string class_code = row[0];
+        int loc_id = std::stoi(row[1]);
+        classes[class_code] = loc_id;
+    }
+}
+
+bool CampusCompass::parseCommand(const std::string& command) {
+    bool is_valid = false;
+    std::vector<std::string> args;
+    std::stringstream ss(command);
+    std::string arg;
+
+    //grabbing what the command is
+    std::string cmd;
+    ss >> cmd;
+
+    //adding all the arguments into a list
+    while (ss >> arg) {
+        args.push_back(arg);
+    }
+
+    //executing the corresponding method based on the command
+    int call = -1;
+    if (!commands.count(cmd)) {
+        call = commands.at(cmd);
+    }
+    switch (call) {
+        case 0:
+            is_valid = insertStudent(args);
+            break;
+        case 1:
+            is_valid = removeStudent(args);
+            break;
+        case 2:
+            is_valid = dropClass(args);
+            break;
+        case 3:
+            is_valid = removeClass(args);
+            break;
+        case 4:
+            is_valid = replaceClass(args);
+            break;
+        case 5:
+            is_valid = toggleEdgesClosure(args);
+            break;
+        case 6:
+            is_valid = checkEdgeStatus(args);
+            break;
+        case 7:
+            is_valid = isConnected(args);
+            break;
+        case 8:
+            is_valid = printShortestEdges(args);
+            break;
+        case 9:
+            is_valid = printStudentZone(args);
+            break;
+    }
     return is_valid;
+}
+
+std::unordered_map<int, std::pair<int, int>> CampusCompass::shortestDistance(int start) {
+    //out map will be in the form of <to, <distance, pre>>
+
+    //initializing values
+    std::unordered_set<int> not_done;
+    std::unordered_map<int, std::pair<int, int>> result;
+    for (auto& loc : graph) {
+        if (loc.first == start) {
+            result[start] = std::make_pair(0, 0);
+        } else {
+            result[loc.first] = std::make_pair(INT_MAX, 0);
+        }
+        not_done.insert(loc.first);
+    }
+
+    //Dijkstras alg
+    while (!not_done.empty()) {
+        //find the vertex with the shortest distance still in not_done set
+        auto init = not_done.begin();
+        int minID = *init;
+        for (int loc : not_done) {
+            if (result[loc].first < result[minID].first && not_done.count(loc)) {
+                minID = loc;
+            }
+        }
+        not_done.erase(minID);
+        //relax each of its edges, relaxation code adapted from "Graphs 2 Review_revised" study guide doc on canvas
+        for (auto loc : graph[minID]) {
+            int to = std::get<0>(loc);
+            int dist = std::get<1>(loc);
+            bool closed = std::get<2>(loc);
+            if (!closed && result[to].first < result[minID].first + dist) {
+                result[to].first = result[minID].first + dist;
+                result[to].second = minID;
+            }
+        }
+    }
+    return result;
+}
+
+bool CampusCompass::validName(std::string name) {
+    //using regex developed on https://regex101.com
+    return std::regex_match(name, std::regex("^[A-Za-z\\s]+$"));
+}
+
+bool CampusCompass::validUFID(std::string id) {
+    return std::regex_match(id, std::regex("^[0-9]{8}$"));
+}
+
+bool CampusCompass::validClassCode(std::string code) {
+    return std::regex_match(code, std::regex("^[A-Z]{3}[0-9]{4}$"));
 }
--- src/CampusCompass.h
+++ src/CampusCompass.h
@@ -1,9 +1,15 @@
 #pragma once
+
 #include <string>
 #include <unordered_map>
 #include <map>
 #include <vector>
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <climits>
+#include <unordered_set>
+#include <regex>
 
 
 class CampusCompass {
@@ -17,9 +23,11 @@ private:
         std::string residence;
         //list of their classes and the shortest time from their residence
         std::map<std::string, int> classes;
+        //list of shortest path edges to desired class in order dijkstra's alg (from, list of proceeding edges)
+        std::unordered_map<int, std::vector<int>> paths;
     };
 
-    //an adjacency list in the form of <id_from, vector<pair<id_to, weight/time, closure>>>
+    //an adjacency list in the form of <id_from, vector<tuple<id_to, weight/time, closure>>>
     std::unordered_map<int, std::vector<std::tuple<int, int, bool>>> graph;
 
     //list of classes
@@ -28,53 +36,59 @@ private:
     //list of students accessible via their id
     std::map<std::string, Student> students;
 
-    //simple list for availible commands
+    //simple list for available commands
     std::unordered_map<std::string, int> commands;
 public:
     // Think about what helper functions you will need in the algorithm
-    CampusCompass(); // constructor
+    CampusCompass(const std::string& edges_filepath, const std::string& classes_filepath); // constructor
 
     //reads the CSV file and converts it into 2D array, code taken from guide on canvas
-    std::vector<std::vector<std::string>> readCSV(std::string& filename);
+    static std::vector<std::vector<std::string>> readCSV(const std::string& filename);
 
     //parses through data and fills in graph and classes list
-    bool parseCSV(const std::string &edges_filepath, const std::string &classes_filepath);
+    void parseCSV(const std::string& edges_filepath, const std::string& classes_filepath);
 
     //checks command validity then executes function
-    bool parseCommand(const std::string &command);
+    bool parseCommand(const std::string& command);
 
     //checks if correct args then adds student to students list
-    bool insertStudent(std::vector<std::string> args);
+    bool insertStudent(std::vector<std::string>& args);
 
     //checks if correct args then removes student from the list
-    bool removeStudent(std::vector<std::string> args);
+    bool removeStudent(std::vector<std::string>& args);
 
     //checks if correct args then drops the sepcified class for that student
-    bool dropClass(std::vector<std::string> args);
+    bool dropClass(std::vector<std::string>& args);
 
     //checks if correct args then replaces classes as specified for that student
-    bool replaceClass(std::vector<std::string> args);
+    bool replaceClass(std::vector<std::string>& args);
 
     //checks args and removes class from every student, outputs how many times removed
-    bool removeClass(std::vector<std::string> args);
+    bool removeClass(std::vector<std::string>& args);
 
     //checks args and toggles edges closure
-    bool toggleEdgesClosure(std::vector<std::string> args);
+    bool toggleEdgesClosure(std::vector<std::string>& args);
 
     //checks args and checks edges status
-    bool checkEdgeStatus(std::vector<std::string> args);
+    bool checkEdgeStatus(std::vector<std::string>& args);
 
     //checks args then checks if connected
-    bool isConnected(std::vector<std::string> args);
+    bool isConnected(std::vector<std::string>& args);
 
     //checks args then prints student's shortest path
-    bool printShortestEdges(std::vector<std::string> args);
+    bool printShortestEdges(std::vector<std::string>& args);
 
     //checks args then prints the studnets zone
-    bool printStudentZone(std::vector<std::string> args);
+    bool printStudentZone(std::vector<std::string>& args);
 
     //Dijkstra's alg for shortest distance to every node from start
     std::unordered_map<int, std::pair<int, int>> shortestDistance(int start);
 
+    //updates students shortest distance data
     void updateStudent(std::string id);
+
+    //following functions are all for data validation
+    bool validUFID(std::string id);
+    bool validName(std::string name);
+    bool validClassCode(std::string code);
 };

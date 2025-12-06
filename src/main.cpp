46f3c58baed0331a2d96fedea96938fa488301ad
Henry Canapp <henrycanapp@ufl.edu>
2025-12-06 12:36:20 -0500

    edited main so that it will skip empty lines

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,18 @@ int main() {
     // the below is example code for parsing commandline input
     //I am using the example code
     int no_of_lines;
-    std::string command;
+    std::string command = "";
     std::cin >> no_of_lines;
     std::cin.ignore(); // ignore newline that first cin left over
 
     std::stringstream output;
     for (int i = 0; i < no_of_lines; i++) {
-        std::getline(std::cin, command);
-
+        while (!command.compare("")) {
+            std::getline(std::cin, command);
+        }
         // parse commands and add to the output
         compass.parseCommand(command, output);
+        command = "";
 
     }
     std::cout << output.str() << std::endl;
